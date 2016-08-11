1f8bb27754b8850e178d1dc9c6c8dca74f95889a
Christian Fisch <christian.fiesel@gmail.com>
2016-08-12 00:41:58 +0200

    add possibility to create tiles from selected png

--- addons/tileset_dock/tileset_dock_plugin.gd
+++ addons/tileset_dock/tileset_dock_plugin.gd
@@ -4,17 +4,36 @@ extends EditorPlugin
 var dock = null
 var tileSize = 64
 var getPolygonFromCollision = true
+var checkCollision = false
+var checkNavigation = false
+var checkImage = false
+var checkOccluder = false
+var mainGuiPath = "VBoxContainer/PanelContainer/VBoxContainer/"
+var imagesPath
+var fileDialog = null
 
 func _enter_tree():
 	dock = preload("res://addons/tileset_dock/tileset_dock.tscn").instance()
-	dock.get_node("VBoxContainer/PanelContainer/VBoxContainer/collisionPolygon").connect("pressed",self,"collisionPolygon")
-	dock.get_node("VBoxContainer/PanelContainer/VBoxContainer/collisionShape").connect("pressed",self,"collisionShape")
-	dock.get_node("VBoxContainer/PanelContainer/VBoxContainer/occluder").connect("pressed",self,"occluder")
-	dock.get_node("VBoxContainer/PanelContainer/VBoxContainer/navigation").connect("pressed",self,"navigation")
-	dock.get_node("VBoxContainer/HBoxContainer/tilesize").connect("text_changed",self,"tilesize")
-	dock.get_node("VBoxContainer/HBoxContainer/tilesize").set_text(str(tileSize))
-	dock.get_node("VBoxContainer/PanelContainer/VBoxContainer/CheckGetPolyColli").connect("toggled",self,"checkPolygonCollision")
-	dock.get_node("VBoxContainer/PanelContainer/VBoxContainer/CheckGetPolyColli").set_toggle_mode(getPolygonFromCollision)
+	#image
+	dock.get_node(mainGuiPath+"HBoxImage/VBoxImage/size/x").connect("text_changed",self,"tilesize")
+	dock.get_node(mainGuiPath+"HBoxImage/VBoxImage/size/x").set_text(str(tileSize))
+	dock.get_node(mainGuiPath+"HBoxImage/CheckBox").connect("toggled",self,"setImageCheck")
+	#dialog
+	dock.get_node(mainGuiPath+"HBoxImage/ImageContainer/btnImage").connect("pressed",self,"show_dialog")
+	#collision
+	dock.get_node(mainGuiPath+"HBoxCollision/collisionPolygon").connect("pressed",self,"collisionPolygon")
+	dock.get_node(mainGuiPath+"HBoxCollision/CheckBox").connect("toggled",self,"setCollisionPolygonCheck")
+	#navigation
+	dock.get_node(mainGuiPath+"HBoxNavigation/navigation").connect("pressed",self,"navigation")
+	dock.get_node(mainGuiPath+"HBoxNavigation/CheckBox").connect("toggled",self,"setNavigationCheck")
+	#occluder
+	dock.get_node(mainGuiPath+"HBoxOccluder/occluder").connect("pressed",self,"occluder")
+	dock.get_node(mainGuiPath+"HBoxOccluder/CheckBox").connect("toggled",self,"setOccluderCheck")
+	#settings
+	dock.get_node(mainGuiPath+"HBoxSettings/CheckGetPolyColli").connect("toggled",self,"setGetPolygonFromCollisionCheck")
+	dock.get_node(mainGuiPath+"HBoxSettings/CheckGetPolyColli").set_toggle_mode(getPolygonFromCollision)
+	#tiles
+	dock.get_node(mainGuiPath+"create_tiles").connect("pressed",self,"create_tiles")
 	add_control_to_dock( DOCK_SLOT_RIGHT_BL, dock )
 
 func _exit_tree():
@@ -24,67 +43,60 @@ func _exit_tree():
 func collisionPolygon():
 	print("add/remove collisionPolygon")
 	for n in get_selection().get_selected_nodes():
-		if n.get_owner() != null:
-			var owner = n.get_owner()
-			var _node = setStaticBody(n,owner)
-			var _node2 = CollisionPolygon2D.new()
-			_node2.set_polygon(getVector2ArrayFromSprite(n))
-			_node2.set_name("CollisionPolygon2D")
-			_node.add_child(_node2)
-			_node2.set_owner(owner)
-		else:
-			print("Error: root node selected")
+		setCollisionPolygon(n)
 
-func collisionShape():
-	print("add/remove collisionShape")
-	for n in get_selection().get_selected_nodes():
-		if n.get_owner() != null:
-			var owner = n.get_owner()
-			var _node = setStaticBody(n,owner)
-			var _node2 = CollisionShape2D.new()
-			var _recShape = RectangleShape2D.new()
-			_recShape.set_extents(Vector2(tileSize/2,tileSize/2))
-			_node2.set_shape(_recShape)
-			_node2.set_name("CollisionShape2D")
-			_node.add_child(_node2)
-			_node2.set_owner(owner)
-		else:
-			print("Error: root node selected")
+func setCollisionPolygon(n):
+	if n.get_owner() != null:
+		var owner = n.get_owner()
+		var _node = setStaticBody(n,owner)
+		var _node2 = CollisionPolygon2D.new()
+		_node2.set_polygon(getVector2ArrayFromSprite(n))
+		_node2.set_name("CollisionPolygon2D")
+		_node.add_child(_node2)
+		_node2.set_owner(owner)
+	else:
+		print("Error: root node selected")
 
 func occluder():
 	print("add/remove occluder")
 	for n in get_selection().get_selected_nodes():
-		if n.get_owner() != null:
-			if n.get_type() == "Sprite":
-				if n.has_node("LightOccluder2D"):
-					print("deleting LightOccluder2D")
-					n.remove_child(n.get_node("LightOccluder2D"))
-				var _node = LightOccluder2D.new()
-				_node.set_occluder_polygon(getOccPolygon2D(n))
-				_node.set_name("LightOccluder2D")
-				n.add_child(_node)
-				_node.set_owner(n.get_parent())
-			else:
-				print("Error: no sprite selected")
+		setOccluder(n)
+
+func setOccluder(n):
+	if n.get_owner() != null:
+		if n.get_type() == "Sprite":
+			if n.has_node("LightOccluder2D"):
+				print("deleting LightOccluder2D")
+				n.remove_child(n.get_node("LightOccluder2D"))
+			var _node = LightOccluder2D.new()
+			_node.set_occluder_polygon(getOccPolygon2D(n))
+			_node.set_name("LightOccluder2D")
+			n.add_child(_node)
+			_node.set_owner(n.get_parent())
 		else:
-			print("Error: root node selected")
-		
+			print("Error: no sprite selected")
+	else:
+		print("Error: root node selected")
+
 func navigation():
 	print("add/remove navigation")
 	for n in get_selection().get_selected_nodes():
-		if n.get_owner() != null:
-			if n.get_type() == "Sprite":
-				if n.has_node("NavigationPolygonInstance"):
-					n.remove_child(n.get_node("NavigationPolygonInstance"))
-				var _node = NavigationPolygonInstance.new()
-				_node.set_navigation_polygon(getNavPolygon(n))
-				_node.set_name("NavigationPolygonInstance")
-				n.add_child(_node)
-				_node.set_owner(n.get_parent())
-			else:
-				print("Error: no sprite selected")
+		setNavigation(n)
+
+func setNavigation(n):
+	if n.get_owner() != null:
+		if n.get_type() == "Sprite":
+			if n.has_node("NavigationPolygonInstance"):
+				n.remove_child(n.get_node("NavigationPolygonInstance"))
+			var _node = NavigationPolygonInstance.new()
+			_node.set_navigation_polygon(getNavPolygon(n))
+			_node.set_name("NavigationPolygonInstance")
+			n.add_child(_node)
+			_node.set_owner(n.get_parent())
 		else:
-			print("Error: root node selected")
+			print("Error: no sprite selected")
+	else:
+		print("Error: root node selected")
 
 func getVector2ArrayFromSprite(selectedNode):
 	var _Array = []
@@ -124,15 +136,124 @@ func tilesize(newTileSize):
 	print("set tilesize to: ", newTileSize)
 	tileSize = int(newTileSize)
 
-func checkPolygonCollision(newValue):
-	print("set check to: ", newValue)
-	getPolygonFromCollision = newValue
-
 func setStaticBody(selectedNode,owner):
 	if selectedNode.has_node("StaticBody2D"):
 		selectedNode.remove_child(selectedNode.get_node("StaticBody2D"))
 	var _node = StaticBody2D.new()
 	_node.set_name("StaticBody2D")
+	var _cps = ConvexPolygonShape2D.new()
+	_cps.set_points(getVector2ArrayFromSprite(selectedNode))
+	_node.clear_shapes()
+	_node.add_shape(_cps)
 	selectedNode.add_child(_node)
 	_node.set_owner(owner)
 	return _node
+
+func show_dialog():
+	if fileDialog == null:
+		fileDialog = FileDialog.new()
+		get_parent().add_child(fileDialog)
+		
+	fileDialog.set_mode(FileDialog.MODE_OPEN_FILES)
+	fileDialog.set_current_path("res://")
+	fileDialog.set_access(FileDialog.ACCESS_RESOURCES)
+	fileDialog.clear_filters()
+	fileDialog.add_filter("*.png ; PNG Images");
+	fileDialog.set_custom_minimum_size(Vector2(500,500))
+	fileDialog.popup_centered()
+	fileDialog.show()
+	if not fileDialog.is_connected("files_selected",self,"on_files_selected"):
+		fileDialog.connect("files_selected",self,"on_files_selected")
+
+func setCollisionPolygonCheck(newValue):
+	print("set checkCollision to: ", newValue)
+	checkCollision = newValue
+	
+func setImageCheck(newValue):
+	print("set checkImage to: ", newValue)
+	checkImage = newValue
+
+func setNavigationCheck(newValue):
+	print("set checkNavigation to: ", newValue)
+	checkNavigation = newValue
+
+func setOccluderCheck(newValue):
+	print("set checkOccluder to: ", newValue)
+	checkOccluder = newValue
+
+func setGetPolygonFromCollisionCheck(newValue):
+	print("set getPolygonFromCollision to: ", newValue)
+	getPolygonFromCollision = newValue
+
+func on_files_selected(_aPath):
+	imagesPath = _aPath
+	var image  = ImageTexture.new()
+	var imageName
+	var imageSize
+	dock.get_node(mainGuiPath+"HBoxImage/CheckBox").set_pressed(true)
+	setImageCheck(true)
+	if _aPath.size() == 1:
+		image.load(_aPath[0])
+		imageSize = image.get_width()
+		if image.get_width() > 64 || image.get_height() > 64:
+			image.set_size_override(Vector2(64,64))
+		imageName = getFileName(_aPath[0])
+	else:
+		imageSize = ""
+		image.load("res://addons/tileset_dock/multiple.png")
+		imageName = "..."
+	
+	dock.get_node(mainGuiPath+"HBoxImage/ImageContainer/TextureFrame").set_texture(image)
+	dock.get_node(mainGuiPath+"HBoxImage/VBoxImage/size/x").set_text(str(imageSize))
+	#dock.get_node(mainGuiPath+"HBoxImage/VBoxImage/size/y").set_text(str(image.get_height()))
+	dock.get_node(mainGuiPath+"HBoxImage/VBoxImage/name/lblName").set_text(imageName)
+
+func getFileName(_path):
+	var _fileName = _path.substr(_path.find_last("/")+1, _path.length() - _path.find_last("/")-1)
+	var _dotPos = _fileName.find_last(".")
+	if _dotPos != -1:
+		_fileName = _fileName.substr(0,_dotPos)
+	return _fileName
+
+func create_tiles():
+	if checkImage:
+		addImageNodes()
+	else:
+		if checkCollision:
+			collisionPolygon()
+		if checkNavigation:
+			navigation()
+		if checkOccluder:
+			occluder()
+
+func addImageNodes():
+	print("creating ",imagesPath.size()," sprites from selection")
+	var _root =  get_tree().get_edited_scene_root()
+	for _path in imagesPath:
+		var _image  = ImageTexture.new()
+		_image.load(_path)
+		tileSize = _image.get_width()
+		var _imageName = getFileName(_path)
+		var _spriteNode
+		if !_root.has_node(_imageName):
+			_spriteNode = Sprite.new()
+			_spriteNode.set_texture(_image)
+			_root.add_child(_spriteNode)
+			_spriteNode.set_pos(Vector2(0,0))
+			_spriteNode.set_owner(_root)
+			_spriteNode.set_name(_imageName)
+		else:
+			_spriteNode = _root.get_node(_imageName)
+			_spriteNode.set_texture(_image)
+		if checkCollision:
+			setCollisionPolygon(_spriteNode)
+		if checkNavigation:
+			setNavigation(_spriteNode)
+		if checkOccluder:
+			setOccluder(_spriteNode)
+	
+	
+	
+	
+	
+	
